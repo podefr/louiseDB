0eaf6b49183100df047be94d1ab1dd77be5e0a62
Olivier Scherrer <pode.fr@gmail.com>
2017-09-05 01:02:23 -0400

    Adding debug statements to persistence layer

--- src/Persistence.js
+++ src/Persistence.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { promisify } = require('util');
 
+const log = require('./log');
+
 const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
 
@@ -11,6 +13,7 @@ module.exports = class Persistence {
 
     async persist(data) {
         if (this.hasFileName()) {
+            log.debug(`Persisting current DB state into ${this.fileName}`);
             return await writeFile(this.fileName, JSON.stringify(data));
         } else {
             return Promise.resolve();
@@ -20,6 +23,7 @@ module.exports = class Persistence {
     async load() {
         if (this.hasFileName()) {
             try {
+                log.debug(`Reloading ${this.fileName} into DB state`);                
                 return JSON.parse(await readFile(this.fileName));
             } catch (err) {
                 return Promise.resolve();
