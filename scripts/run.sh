d24bd49406801b5237d312c5492773f6a5a6c890
Olivier Scherrer <pode.fr@gmail.com>
2018-12-09 00:47:45 -0500

    Update npm packages to fix vulnerabilities

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-docker run  -u`id -u`:`id -g` 
-            --mount src=`pwd`,target=/mnt,type=bind
+docker run  -u`id -u`:`id -g` \
+            --mount src=`pwd`,target=/mnt,type=bind \
             -it louisedb-npm $@
